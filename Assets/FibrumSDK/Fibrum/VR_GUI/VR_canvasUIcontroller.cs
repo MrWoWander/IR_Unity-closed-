1e53b448d180d62821e5f2ed2c53e774818b8e7b
Mikhail <wowivan2011@yandex.ru>
2017-02-13 23:24:05 +0300

    Meta 222222
    Пару ебаных мет и еще пару Fibrum SDK файлов

---
+++ Assets/FibrumSDK/Fibrum/VR_GUI/VR_canvasUIcontroller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+public class VR_canvasUIcontroller : NetworkBehaviour {
+
+	Camera UI_dummyCamera;
+	public float lookToPressTime=2f;
+	public Sprite defaultProgressBarTex;
+	public Texture defaultPointerTex;
+
+	public void UpdatesSceneCanvases()
+	{
+		if (isLocalPlayer) {
+		//UI_dummyCamera = transform.Find("VRCamera/VR_UI_dummyCamera").GetComponent<Camera>();
+			UI_dummyCamera = gameObject.transform.GetChild (0).GetChild (0).GetChild(2).GetComponent<Camera> ();
+			GameObject eventSystem = null;
+			if (GameObject.FindObjectOfType<EventSystem> () != null)
+				eventSystem = GameObject.FindObjectOfType<EventSystem> ().gameObject;
+			if (eventSystem != null) {
+				if (eventSystem.GetComponent<VRInputModule> () == null) {
+					if (eventSystem.GetComponent<StandaloneInputModule> () != null)
+						eventSystem.GetComponent<StandaloneInputModule> ().enabled = false;
+					if (eventSystem.GetComponent<TouchInputModule> () != null)
+						eventSystem.GetComponent<TouchInputModule> ().enabled = false;
+					VRInputModule vrim = eventSystem.AddComponent<VRInputModule> ();
+					vrim.enabled = true;
+					vrim.timeToLookPress = lookToPressTime;
+					vrim.SetProgressBarTexture (defaultProgressBarTex);
+					vrim.SetVRpointerTexture (defaultPointerTex);
+				}
+				Canvas[] cvs = GameObject.FindObjectsOfType<Canvas> ();
+				for (int k = 0; k < cvs.Length; k++) {
+					if (cvs [k].renderMode == RenderMode.WorldSpace)
+						cvs [k].worldCamera = UI_dummyCamera;
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		Invoke("UpdatesSceneCanvases",0f);
+	}
+}
